50697d61428cbaaebabb77e6d90479208e9510ea
AkimotoYuhi <chiisanaaki@gmail.com>
2022-06-08 20:28:40 +0900

    CardDataに効果用の項目を追加

--- Assets/Scripts/Charactor/CardData.cs
+++ Assets/Scripts/Charactor/CardData.cs
@@ -16,8 +16,36 @@ public class CardDataBase
     [SerializeField] Sprite m_icon;
     [SerializeField] string m_cost;
     [SerializeField, TextArea] string m_tooltip;
+    [SerializeReference, SubclassSelector] List<ICommand> m_cardCommand;
     public string Name => m_name;
     public Sprite Icon => m_icon;
     public string Cost => m_cost;
     public string Tooltip => m_tooltip;
+    public List<ICommand> CardCommand => m_cardCommand;
+}
+
+public interface ICommand
+{
+    void Execute();
+}
+
+public class AttackCommand : ICommand
+{
+    [SerializeField] UseType m_useType;
+    [SerializeField] int m_power;
+    [SerializeField] bool m_isTrueDamage;
+
+    public void Execute()
+    {
+
+    }
+}
+
+public enum UseType
+{
+    None = -1,
+    Player,
+    Enemy,
+    AllEnemies,
+    System,
 }
---
+++ Assets/Scripts/Charactor/Command.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct Command
+{
+    public int Power { get; set; }
+    public int Block { get; set; }
+    //public List<Effect> Effect { get; set; }
+    public UseType UseType { get; set; }
+}
