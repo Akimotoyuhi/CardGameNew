c67fe5626c58e444550e703bd7df65636c650ec8
AkimotoYuhi <chiisanaaki@gmail.com>
2022-06-12 21:52:48 +0900

    山札から引く機能途中

--- Assets/Scripts/System/Deck.cs
+++ Assets/Scripts/System/Deck.cs
@@ -7,10 +7,30 @@ using UnityEngine;
 /// </summary>
 public class Deck : CardControllerBase
 {
-    public int DrowCardNum { get; set; }
-
     public override void Setup()
     {
         base.Setup();
+    }
+
+    public void Draw(int drawNum)
+    {
+        for (int i = 0; i < drawNum; i++)
+        {
+            if (ChildCount <= 0)
+            {
+                Debug.Log("山札切れ");
+                //m_discard.ConvartToDeck(); //山札が無かったら捨て札からカードを戻す
+                if (ChildCount == 0)
+                {
+                    Debug.Log("デッキ枚数不足"); //捨て札からカードを戻しても山札がないなら引くのをやめる
+                    return;
+                }
+            }
+            int r = Random.Range(0, ChildCount);
+            //Card b = m_cardParent.GetChild(r).GetComponent<Card>();
+            //b.CardState = CardState.Play;
+            //b.GetPlayerEffect();
+            //m_cardParent.GetChild(r).SetParent(m_hand.CardParent, false);
+        }
     }
 }
